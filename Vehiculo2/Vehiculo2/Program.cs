3a4708dacb589d5d9bcacd1397575cab21ac3067
Alvarking234 <alvarinmorales23@gmail.com>
2026-09-06 01:32:46 -0600

    Add files via upload

---
+++ Vehiculo2/Vehiculo2/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Automovil : Vehiculo
+{
+    public Automovil(int codigo, string placa, string marca,
+                     string modelo, double capacidad,
+                     double costoOperativo)
+        : base(codigo, placa, marca, modelo, capacidad, costoOperativo)
+    {
+    }
+
+    public override bool PuedeTransportar(double peso)
+    {
+        return peso <= Capacidad;
+    }
+
+    public override void MostrarInformacion()
+    {
+        Console.WriteLine("Tipo: Automóvil");
+        base.MostrarInformacion();
+    }
+}
