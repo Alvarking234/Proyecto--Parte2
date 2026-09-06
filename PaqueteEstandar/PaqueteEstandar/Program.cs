a3894fc327ba7051c27fcb9a43a0447c29534225
Alvarking234 <alvarinmorales23@gmail.com>
2026-09-06 01:39:38 -0600

    Add files via upload

---
+++ PaqueteEstandar/PaqueteEstandar/Program.cs
@@ -0,0 +1,21 @@
+﻿public class PaqueteEstandar : Paquete
+{
+    public PaqueteEstandar(int codigo, string descripcion, double peso,
+                           double valorDeclarado, string direccionOrigen,
+                           string direccionDestino)
+        : base(codigo, descripcion, peso, valorDeclarado,
+               direccionOrigen, direccionDestino, "Paquete estándar")
+    {
+    }
+
+    public override double CalcularTarifa(double distancia)
+    {
+        return 25 + (distancia * 4) + (Peso * 2);
+    }
+
+    public override void MostrarInformacion()
+    {
+        Console.WriteLine("Tipo: Paquete estándar");
+        base.MostrarInformacion();
+    }
+}
