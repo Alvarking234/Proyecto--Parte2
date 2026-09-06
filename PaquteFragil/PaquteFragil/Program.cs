975f86ddc5a0470668605c898dde8895a4a5a3f1
Alvarking234 <alvarinmorales23@gmail.com>
2026-09-06 01:40:03 -0600

    Add files via upload

---
+++ PaquteFragil/PaquteFragil/Program.cs
@@ -0,0 +1,21 @@
+﻿public class PaqueteFragil : Paquete
+{
+    public PaqueteFragil(int codigo, string descripcion, double peso,
+                         double valorDeclarado, string direccionOrigen,
+                         string direccionDestino)
+        : base(codigo, descripcion, peso, valorDeclarado,
+               direccionOrigen, direccionDestino, "Paquete frágil")
+    {
+    }
+
+    public override double CalcularTarifa(double distancia)
+    {
+        return 35 + (distancia * 5) + (Peso * 3);
+    }
+
+    public override void MostrarInformacion()
+    {
+        Console.WriteLine("Tipo: Paquete frágil");
+        base.MostrarInformacion();
+    }
+}
