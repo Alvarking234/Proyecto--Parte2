3dd2b763e7fdd0a3c128cfd73eb1b87904b2f7f6
Alvarking234 <alvarinmorales23@gmail.com>
2026-09-06 01:40:32 -0600

    Add files via upload

---
+++ Paqueterefrigerado/Paqueterefrigerado/Program.cs
@@ -0,0 +1,21 @@
+﻿public class ProductoRefrigerado : Paquete
+{
+    public ProductoRefrigerado(int codigo, string descripcion, double peso,
+                               double valorDeclarado, string direccionOrigen,
+                               string direccionDestino)
+        : base(codigo, descripcion, peso, valorDeclarado,
+               direccionOrigen, direccionDestino, "Producto refrigerado")
+    {
+    }
+
+    public override double CalcularTarifa(double distancia)
+    {
+        return 40 + (distancia * 6) + (Peso * 4);
+    }
+
+    public override void MostrarInformacion()
+    {
+        Console.WriteLine("Tipo: Producto refrigerado");
+        base.MostrarInformacion();
+    }
+}
