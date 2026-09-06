c5043feea9a81ae0e62cabaea38ee8ebb1545c27
Alvarking234 <alvarinmorales23@gmail.com>
2026-09-06 01:39:14 -0600

    Add files via upload

---
+++ documento/documento/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Documento : Paquete
+{
+    public Documento(int codigo, string descripcion, double peso,
+                     double valorDeclarado, string direccionOrigen,
+                     string direccionDestino)
+        : base(codigo, descripcion, peso, valorDeclarado,
+               direccionOrigen, direccionDestino, "Documento")
+    {
+    }
+
+    public override double CalcularTarifa(double distancia)
+    {
+        return 20 + (distancia * 3);
+    }
+
+    public override void MostrarInformacion()
+    {
+        Console.WriteLine("Tipo: Documento");
+        base.MostrarInformacion();
+    }
+}
