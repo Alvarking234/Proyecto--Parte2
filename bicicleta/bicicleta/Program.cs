5b041bc81eb623708239a195ec74614aecee105d
Alvarking234 <alvarinmorales23@gmail.com>
2026-09-06 01:32:22 -0600

    Add files via upload

---
+++ bicicleta/bicicleta/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Bicicleta : Vehiculo
+{
+    public Bicicleta(int codigo, string marca, string modelo,
+                     double capacidad, double costoOperativo)
+        : base(codigo, "", marca, modelo, capacidad, costoOperativo)
+    {
+    }
+
+    public override bool PuedeTransportar(double peso)
+    {
+        return peso <= Capacidad;
+    }
+
+    public override void MostrarInformacion()
+    {
+        Console.WriteLine("Tipo: Bicicleta");
+        base.MostrarInformacion();
+    }
+}
