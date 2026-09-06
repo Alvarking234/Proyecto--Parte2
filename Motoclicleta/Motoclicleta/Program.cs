2d3d546ff15166a01578aa7ac6b04f6087e7ac7f
Alvarking234 <alvarinmorales23@gmail.com>
2026-09-06 01:31:53 -0600

    Add files via upload

---
+++ Motoclicleta/Motoclicleta/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Motocicleta : Vehiculo
+{
+    public Motocicleta(int codigo, string placa, string marca,
+                       string modelo, double capacidad,
+                       double costoOperativo)
+        : base(codigo, placa, marca, modelo, capacidad, costoOperativo)
+    {
+    }
+
+    public override bool PuedeTransportar(double peso)
+    {
+        return peso <= Capacidad;
+    }
+
+    public override void MostrarInformacion()
+    {
+        Console.WriteLine("Tipo: Motocicleta");
+        base.MostrarInformacion();
+    }
+}
